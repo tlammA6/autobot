3409064614c126c56cbc940b1374f9d2fe3ecfc0
tlammA6 <theron.lamm@binarystar.biz>
2021-04-30 20:22:11 -0400

    initial project commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, inject, Inject } from '@angular/core';
+import { Interactions } from 'aws-amplify';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'autobot';
+  
+export class AppComponent implements OnInit {
+  
+  title = 'AutoBot';
+  conversation: string = '';
+  message: string = '';
+
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+  }
+
+  async startChat() {
+    // Provide a bot name and user input
+    this.conversation = this.conversation + "\nYou::" + this.message;
+    var response = await Interactions.send("autobot_dev", this.message.toString());
+    //Log chatbot response
+    console.log(response);
+    this.message = '';
+    if(response && response.message){
+      this.conversation = this.conversation + "\nBot::" + response.message
+    }
+    if(response && !response.message){
+      this.conversation = this.conversation + "\nBot::" + "Your Hotel Room Booking is complete."
+
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Amplify, {Interactions } from 'aws-amplify';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-1:c10c1e33-b691-4533-a00f-88bc58cd7416',
+    region: 'us-east-1'
+  },
+  Interactions: {
+    bots: {
+      "autobot_dev": {
+        "name": "autobot_dev",
+        "alias": "$LATEST",
+        "region": "us-east-1",
+      },
+    }
+  }
+});
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
